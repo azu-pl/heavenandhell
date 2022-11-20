1f2ddc2a89288fa8c4b6f683b5fd42965735621f
Paweł Malinoś <pawel_malinos@o2.pl>
2022-11-20 13:51:26 +0100

    Add new models.

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -43,3 +43,12 @@ class Staff(Base):
     phone = Column(String(20))
     adress = Column(String(50))
     enrollment_data = Column(DateTime, nullable=False, default=datetime.now)
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    budget = Column(Float, nullable=False)
+    address = Column(String(50))
